0c7886f6fb5e8ad613b4fbbee3bf018a1b75482e
EloraBarua <elorabarua62@gmail.com>
2022-05-05 01:52:03 +0600

    Database connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // Middleware
@@ -9,6 +10,27 @@ app.use(cors());
 app.use(express.json());
 
 
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.toxng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run()
+{
+    try{
+        await client.connect();
+        const booksCollection = client.db('BookPile').collection('books');
+
+    }
+    finally
+    {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+// Main route
 app.get('/', (req,res) => {
     res.send("BookPile server is running");
 })
